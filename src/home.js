3d89317d3d1051cf6fbd9a3f2b11f5f2c1d2bfe0
Ryan Hetherington <hetheringtonr2@comcast.net>
2022-04-17 14:47:59 -0700

    fixed error with JSON

--- src/home.js
+++ src/home.js
@@ -1,63 +1,6 @@
 //Global Selectors
 const newTaskForm = document.getElementById("newProjectForm");
 
-function createModals() {
-    
-    // Add Task Modal //
-    let myModal = document.querySelector(".modal");
-    let addButton = document.querySelector(".trigger");
-    let closeButton = document.querySelector(".close-btn");
-
-    addButton.onclick = function() {
-        myModal.classList.toggle("show-modal");
-    }
-
-    closeButton.onclick = function() {
-        myModal.classList.toggle("show-modal");
-    }
-
-    //// Edit Modal //
-    //let myEditModal = document.querySelector(".modal2");
-    //let editButton = document.querySelector(".trigger2");
-    //let closeEditButton = document.querySelector(".close-btn2");
-//
-    //editButton.onclick = function() {
-    //    myEditModal.classList.toggle("show-modal2");
-    //}
-//
-    //closeEditButton.onclick = function() {
-    //    myEditModal.classList.toggle("show-modal2");
-    //}
-    //// Add Project Modal //
-    //let myProjectModal = document.querySelector(".modal3");
-    //let addProjectBtn = document.querySelector(".trigger3");
-    //let closeProjectBtn = document.querySelector(".close-btn3");
-    //
-    //addProjectBtn.onclick = function() {
-    //    myProjectModal.classList.toggle(".show-modal3");
-    //}
-    //closeProjectBtn.onclick = function() {
-    //    myProjectModal.classList.toggle(".show-modal3");
-    //}
-}
-
-function createBackground() {
-    // Change Background Image //
-    var randomBackground = function() {
-        var imgArray = ["/dist/images/ashim-d-silva-WeYamle9fDM-unsplash.jpg",
-                        "/dist/images/buzz-andersen-E4944K_4SvI-unsplash.jpg",
-                        "/dist/images/daniel-tong-dxYdPSuBr-U-unsplash.jpg",
-                        "/dist/images/stephen-leonardi-eSNjFDbw_i4-unsplash.jpg",
-                        "/dist/images/maud-bocquillod-bj7EB5my6og-unsplash.jpg",
-                        "/dist/images/john-fowler-9qgKQewttVs-unsplash.jpg"];
-        var getRandomNum = imgArray[Math.floor(Math.random() * (imgArray.length))];
-        document.querySelector("section").style.backgroundImage = 'url('+getRandomNum+')';
-    }
-
-    window.onload = function() {
-        randomBackground();
-    }
-}
 // Create a factory that creates "project" objects.
 // This project object should have at least title adn todos (array od todo items, empty by default).
 // Later add button that will call that function and create new projects.
@@ -74,7 +17,7 @@ class Task {
 class EventHandler {
     static displayTasks() {
         const tasks = Storage.getTasks();
-        tasks.forEach((task)=> EventHandler.addTaskList(task));
+        tasks.forEach((task) => EventHandler.addTaskList(task));
     }
     // Add Task List
     static addTaskList(task) {
@@ -166,7 +109,7 @@ newTaskForm.addEventListener('submit', (e) => {
     }
 });
 
-checkbox = document.getElementById("done");
+let checkbox = document.getElementById("done");
 checkbox.addEventListener('change', (event) => {
     if(event.target.checked) {
         checkbox.value = true;
@@ -174,6 +117,40 @@ checkbox.addEventListener('change', (event) => {
         checkbox.value = false;
     }
 });
+
+function createModals() {
+    
+    // Add Task Modal //
+    let myModal = document.querySelector(".modal");
+    let addButton = document.querySelector(".trigger");
+    let closeButton = document.querySelector(".close-btn");
+
+    addButton.onclick = function() {
+        myModal.classList.toggle("show-modal");
+    }
+
+    closeButton.onclick = function() {
+        myModal.classList.toggle("show-modal");
+    }
+}
+
+function createBackground() {
+    // Change Background Image //
+    var randomBackground = function() {
+        var imgArray = ["/dist/images/ashim-d-silva-WeYamle9fDM-unsplash.jpg",
+                        "/dist/images/buzz-andersen-E4944K_4SvI-unsplash.jpg",
+                        "/dist/images/daniel-tong-dxYdPSuBr-U-unsplash.jpg",
+                        "/dist/images/stephen-leonardi-eSNjFDbw_i4-unsplash.jpg",
+                        "/dist/images/maud-bocquillod-bj7EB5my6og-unsplash.jpg",
+                        "/dist/images/john-fowler-9qgKQewttVs-unsplash.jpg"];
+        var getRandomNum = imgArray[Math.floor(Math.random() * (imgArray.length))];
+        document.querySelector("section").style.backgroundImage = 'url('+getRandomNum+')';
+    }
+
+    window.onload = function() {
+        randomBackground();
+    }
+}
 
 function loadWebsite() {
     createModals();
