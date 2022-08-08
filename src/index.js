d9632522531b39294f04d58553f019262303379d
Ryan Hetherington <hetheringtonr2@comcast.net>
2022-08-08 15:01:17 -0700

    deleted files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import loadWebsite from './home';
-import defaultProject from './project.js';
 import createToDo from './createToDo.js';
-import { loadHeader, loadNewProject } from './loadProjects.js';
 
 loadWebsite();
-defaultProject();
 createToDo();
 
-let newProject = [];
-if (localStorage.getItem('user') == null) {
-    createProject("Test Project", "A Test Project");
-} else {
-    projectLoad = JSON.parse(window.localStorage.getItem('user')) 
-    loadNewProject();
-    loadHeader();
-}
-
-export {newProject};
+// Create Projects //
