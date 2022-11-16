f4be1498702ad84a7c73d97c54a770a7e6e1524f
Ryan Hetherington <hetheringtonr2@comcast.net>
2022-11-16 15:22:17 -0800

    dynamically created elements

--- src/createProject.js
+++ src/createProject.js
@@ -1,18 +1,32 @@
 // Create Tabs
-function onTabClick() {
-    var tabList = document.getElementById("project-tab");
-    tabList.addEventListener('click', active );
-
-    function active(event) {
+const createTabs =()=> { 
+    // Highlight Tabs
+    var list = document.getElementById("project-list");
+    list.addEventListener('click', (event) => {
         if(event.target.tagName === 'A') {
-            tabList.querySelectorAll('li').forEach( el => el.classList.remove('active'));
+            list.querySelectorAll('li').forEach( el => el.classList.remove('active'));
             event.target.parentNode.classList.add('active');
         }
-        console.log("clicked.")
-    }
+        console.log("clicked.");
+    });
+    // Display Tabs
+        var list = document.getElementById("project-list");
+        var row = document.createElement("li");
+        row.innerHTML = `
+        <a href="#">Default <button class="trash"></button></a>` 
+        list.appendChild(row);
 }
+
+function projectTitle() {
+    var taskTitle = document.getElementById("task-title");
+    var paragraph = document.createElement("p");
+    paragraph.innerHTML = " Default Project ";
+    taskTitle.appendChild(paragraph);
+}
+
 const createProject =()=> {
-    onTabClick();
+    projectTitle();
+    createTabs();
 };
 
 export default createProject;
